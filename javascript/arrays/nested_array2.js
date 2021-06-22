1ce81d95498470f37e4afeaec0d8a0fde35a9309
AkhilBhasy <akhil.pandary@gmail.com>
2021-06-22 12:33:00 +0530

    array of objects

---
+++ javascript/arrays/nested_array2.js
@@ -0,0 +1,18 @@
+var products=[
+    [100,"ricepowder",30,10],
+    [101,"oreo",35,100],
+    [102,"darkfantacy",40,50],
+    [103,"fifty",20,5],
+    [104,"horlicks",200,5],
+    [105,"complan",190,0],
+    [106,"boost",250,10],
+    [107,"item1",5,10]
+];
+
+
+for(let pro of products)
+console.log(pro);
+let count=0;
+for(let pro of products)
+ count+=1;
+ console.log(count);
